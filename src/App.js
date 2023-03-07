7bf4d83103900e1dc844d19076b181ae81e27304
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-03-06 23:08:03 -0300

    Trabalhando com States

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import React from 'react'
-import Header from './Header'
-import Post from './Post'
+import React, { useState } from 'react';
+import Header from './Header';
+import Post from './Post';
 
 
-const posts = [
-  {title: 'Title#01', subtitle: 'Sub#01', likes: 20},
-  {title: 'Title#02', subtitle: 'Sub#02', likes: 10},
-  {title: 'Title#03', subtitle: 'Sub#03', likes: 50}
-]
-
+// React trabalha com imutabilidade
 
 export function App() {
-  return(
+const [posts, setPosts] = useState([
+  {id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20},
+  {id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10},
+  {id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50}
+])
+console.log({posts});
+
+function handleRefresh() {
+    setPosts([...posts,
+      {
+        id: Math.random(), 
+        title: `Title#0${posts.length + 1}`, 
+        subtitle: `Sub#0${posts.length + 1}`, 
+        likes: 50
+      }])
+}
+
+  return (
     <>
-  
       <Header>
         <h2>Posts da semana</h2>
+        <button onClick={handleRefresh}>Atualizar</button>
       </Header>
       <hr />
 
         {posts.map((post) => (
           <Post 
-            key={post.title}
+            key={post.id}
             likes={post.likes}
             post={{
               title: post.title,
