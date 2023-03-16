da9488cce762cf9f4ffe2d3968b2136c71f8bc30
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-03-15 22:55:49 -0300

    Definindo vários estilos dentro da mesma função `prop`

--- src/components/Post/styles.js
+++ src/components/Post/styles.js
@@ -1,9 +1,15 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const Container = styled.article`
   margin-bottom: 24px;
-  opacity: ${(props) => (props.removed ? 0.5 : 2)};
-  color: ${(props) => (props.removed ? 'red' : 'white')};
+  /* opacity: ${(props) => (props.removed ? 0.5 : 2)};
+  color: ${(props) => (props.removed ? 'red' : 'white')}; */
+
+  //Usando as props globalmente
+  ${(props) => css`
+    opacity: ${props.removed ? 0.5 : 2};
+    color: ${props.removed ? 'red' : 'white'};
+  `}
 `
 
 export const Subtitle = styled.small`
