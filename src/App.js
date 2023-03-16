33665990734aa08baa2c29a0ccde073ba2587913
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-03-15 23:45:44 -0300

    Implementando a troca de temas part 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import { ThemeProvider } from 'styled-components'
 
 import Layout from './components/Layout'
@@ -7,6 +7,12 @@ import GlobalStyle from './styles/global'
 import themes from './styles/themes'
 
 function App() {
+  const [theme, setTheme] = useState('dark')
+
+  const currentTheme = useMemo(() => {
+    return themes[theme]
+  }, [theme])
+
   return (
     <ThemeProvider theme={themes.light}>
       <GlobalStyle />
