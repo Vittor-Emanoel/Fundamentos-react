16bfa8e8e0a71a642bf44905c356bd1a3fbdba14
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-03-20 14:54:12 -0300

    Transformando o component PostsList para component de class

--- src/components/PostsList/index.js
+++ src/components/PostsList/index.js
@@ -1,20 +1,38 @@
 import React from 'react';
 
-import { Container } from './styles';
 import Post from './Post';
 
 import posts from './posts';
+import { Container } from './styles';
 
-export default function PostsList() {
-  return (
-    <Container>
-      {posts.map((post) => (
-        <Post
-          key={post.id}
-          title={post.title}
-          description={post.description}
-        />
-      ))}
-    </Container>
-  );
+
+//todo component de classe precisa extender essa classe
+export default class PostsList extends React.Component {
+  render() {
+    return (
+          <Container>
+            {posts.map((post) => (
+              <Post
+                key={post.id}
+                title={post.title}
+                description={post.description}
+              />
+            ))}
+          </Container>
+        );
+  }
 }
+
+// export default function PostsList() {
+//   return (
+//     <Container>
+//       {posts.map((post) => (
+//         <Post
+//           key={post.id}
+//           title={post.title}
+//           description={post.description}
+//         />
+//       ))}
+//     </Container>
+//   );
+// }
