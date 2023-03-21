3fbd928de25ba06f22e469b4324a55624ea5feb6
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-03-20 23:29:10 -0300

    Trabalhando com states em component de classe

--- src/App.js
+++ src/App.js
@@ -1,50 +1,84 @@
-import React, { useMemo, useState } from 'react'
+import React, { Component } from 'react'
 import { ThemeProvider } from 'styled-components'
 
 
-
-import themes from './styles/themes'
 //Styles
 import Layout from './components/Layout'
 import GlobalStyle from './styles/global'
+import themes from './styles/themes'
 
 
-function App() {
 
-  // const {theme, currentTheme} = useContext(GlobalContext)
-  const [theme, setTheme] = useState('dark')
-
-
-  const currentTheme = useMemo(() => {
-    return themes[theme] || themes.dark
-  }, [theme])
-
-  function handleToggleTheme() {
-    setTheme((prevState) => (prevState === 'dark' ? 'light' : 'dark'))
+class App extends Component {
+  constructor(props) {
+    super(props)
+   
+    //primeira forma de criar state
+    this.state = {
+      theme: 'dark',
+    }
   }
 
 
-  //função de efeito
+  render() {
+    
 
-  //Primeiro caso de uso é executar na primeira renderização do component 
-  //Executar apenas uma vez depois do mount = API ARRAY VAZIO[]
-  //Executar sempre que um state ou prop mudar = ELE FICA MONITORANDO AS VARIAVÉIS
+    const {theme} = this.state
 
-
-  //unmont = quando eles desmonta
-
-  useState(() => {
-       console.debug('useEffect executou') 
-  }, [])
-
-  return (
-
-    <ThemeProvider theme={currentTheme}>
+    return (
+      
+    <ThemeProvider theme={themes[theme] || themes.dark}>
       <GlobalStyle />
-      <Layout selectedTheme={theme} onToggleTheme={handleToggleTheme} />
+      <Layout 
+      selectedTheme={theme} 
+      onToggleTheme={() => {
+        this.setState(prevState => ({ theme: prevState.theme === 'dark' ? 'light' : 'dark'}))
+      }} />
     </ThemeProvider>
 
-  )
+      )
+  }
 }
+
+
+
+
+// function App() {
+
+//   // const {theme, currentTheme} = useContext(GlobalContext)
+//   const [theme, setTheme] = useState('dark')
+
+
+//   const currentTheme = useMemo(() => {
+//     return themes[theme] || themes.dark
+//   }, [theme])
+
+//   function handleToggleTheme() {
+//     setTheme((prevState) => (prevState === 'dark' ? 'light' : 'dark'))
+//   }
+
+
+//   //função de efeito
+
+//   //Primeiro caso de uso é executar na primeira renderização do component 
+//   //Executar apenas uma vez depois do mount = API ARRAY VAZIO[]
+//   //Executar sempre que um state ou prop mudar = ELE FICA MONITORANDO AS VARIAVÉIS
+
+
+//   //unmont = quando eles desmonta
+
+//   useState(() => {
+//        console.debug('useEffect executou') 
+//   }, [])
+
+//   return (
+
+//     <ThemeProvider theme={currentTheme}>
+//       <GlobalStyle />
+//       <Layout selectedTheme={theme} onToggleTheme={handleToggleTheme} />
+//     </ThemeProvider>
+
+//   )
+// }
 
 export default App
